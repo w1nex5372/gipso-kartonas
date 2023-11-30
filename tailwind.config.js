bbc963ce8af1a1b76f4718e6934de3abafa1500e
Lukas <puras15@gmail.com>
2023-11-30 10:44:44 +0200

    mobile nav menu added

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,17 @@ module.exports = {
   content: ["./dist/**/*.{html,js}"],
   theme: {
     extend: {
+      height: {
+        "50vh": "50vh",
+      },
       colors: {
         primaryBlack: "#262626",
         secondaryWhite: "#FFFFFF",
         thirth: "#988877",
         forth: "#9A8A78",
+      },
+      spacing: {
+        sm: "640px", // Change this value to your desired small spacing
       },
     },
   },
